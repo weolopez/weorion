e12f33417a5a7b3b83063a264d21baa08f9a5069
weolopez <weolopez@gmail.com>
2013-01-04 21:56:27 -0500

    Update weolopez/js/clientSideInclude.js

--- weolopez/js/clientSideInclude.js
+++ weolopez/js/clientSideInclude.js
@@ -7,13 +7,6 @@ function load(twit) {
       columnWidth: 50
     });
     
- /*   $container.masonry({
-      itemSelector: '.box',
-      columnWidth: 240,
-      isAnimated: !Modernizr.csstransitions
-    });
-   */ 
-    // Sites using Masonry markup
     var $sites = $('#sites'),
         $loadingItem = $container.find('#loading');
         
@@ -62,13 +55,15 @@ function updateIS2() {
 			function(index) {
 			    var c = $(this);
 			    var u = c.attr("src");
-	   // 		alert(index + ': ' + u);
 	   			
 			    $(this).load(u);
-			    /*, function(){
-			    	c.append($(this).html());
-			    });*/
+			    
+			    var t = c.attr("twit");
+			    if (t=='weolopez') {
+			    	load(t);
+			    }
 			});
+			
 };
 
 
@@ -82,54 +77,3 @@ function clientSideInclude() {
 };
 
 
-function dynamicLoad(twit, loadingDivID) {
-
-    var $container = $('#container');
-    
-    $('#mini-container').masonry({
-      columnWidth: 50
-    });
-    
- /*   $container.masonry({
-      itemSelector: '.box',
-      columnWidth: 240,
-      isAnimated: !Modernizr.csstransitions
-    });
-   */ 
-    // Sites using Masonry markup
-    var $sites = $('#sites')
-    var $loadingItem = $container.find(loadingDivID);
-        
-    var ajaxError = function(){$loadingItem.text('Could not load examples :(');};
-    
-    // dynamically load content from twitter
-    $.getJSON('https://api.twitter.com/1/statuses/user_timeline.json?screen_name='+twit+'&count=5&callback=?')
-      .error( ajaxError )
-      .success(function( data ){
-        // proceed only if we have data
-        if ( !data || !data.length ) {
-          ajaxError();
-          return;
-        }
-        var items = [],
-            item, datum;
-        for ( var i=0, len = data.length; i < len; i++ ) {
-          datum = data[i];
-          item = '<div class="box col1" ><a href="' + datum.user.url + '">'
-            + '<img style="width:50px;height:65px" src="' + datum.user.profile_image_url.replace('/l.', '/m.') + '" />'
-            + '<b>' + datum.text + '</b>'
-            + '</a></div>';
-          items.push( item );
-        }
-        var $items = $( items.join('') );
-        $items.imagesLoaded(function(){
-         // $container.masonry( 'remove', $('.githubloading' );
-          $('#githubloading').remove();
-          $container.masonry().append( $items ).masonry( 'appended', $items, true );
-        });
-      });
-}
-
-dynamicLoad('orionhub','.githubloading');
-
-//dynamicLoad('twitter','.twitter');
