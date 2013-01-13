93a7f078a398dccf759db49180f25083595ae9a4
weolopez <weolopez@gmail.com>
2013-01-13 15:00:55 -0500

    Update weolopez/js/clientSideInclude.js

--- weolopez/js/clientSideInclude.js
+++ weolopez/js/clientSideInclude.js
@@ -1,4 +1,3 @@
-
 function loadMasonry(component) {
 	
     var $container = $('#container');
@@ -45,6 +44,10 @@ function loadTWIT(twit) {
     };
     var url = 'https://api.twitter.com/1/statuses/user_timeline.json?screen_name='+twit.attr("id")+'&count=5&callback=?';
     
+   // alert('log'+url);
+    // dynamically load content from twitter
+    // Assign handlers immediately after making the request,
+// and remember the jqxhr object for this request
     var jqxhr = $.getJSON(url, function( data ){   
     //	alert('jsondata'+data);
     	   // proceed only if we have data
@@ -82,58 +85,19 @@ function loadTWIT(twit) {
     });
 }
 
-function loadMasonry(component) {
-	
-    var $container = $('#container');
-    window.internallURL = component.attr("src");
-//    alert('URL'+internallURL);
-    var jqxhr = $.ajax({
-                url: internallURL,
-                dataType: "html",
-                complete: function( jqXHR, status ) {
-				if ( callback ) {
-					self.each( callback, response || [ jqXHR.responseText, status, jqXHR ] );
-				}
-                }
-            }).done(function( responseText ) {
-//alert('adding plan'+responseText);
-                var tempHTML = $('<div class="csi box col1" src="'+this.url+'">').html(responseText);
-			    component.remove();
-                $container.masonry().append( tempHTML ).masonry( 'appended', tempHTML, true );
-            }).fail(function() {
-                alert("error");
-            }).always(function() {
-    //            alert("complete");
-            });
-
-// perform other work here ...
-
-// Set another completion function for the request above
-        jqxhr.always(function() {
-         //   alert("second complete");
-        });
-}
-
 function updateIS2() {
-	$(".csi").each(function(index) {
+	$(".csi").each(
+			function(index) {
 			    var c = $(this);
 			    var u = c.attr("src");
 			    c.load(u);
 			   // $container.masonry().append( c ).masonry( 'appended', c, true );
 	});
 
-				var u = c.attr("src");
-			//	c.load(u);
-			    loadMasonry(c);
-	});
-				var u = c.attr("src");
-			//	c.load(u);
-			    loadMasonry(c);
-	});
 }
 
 function loadCSS() {
-	//$('<link rel="stylesheet" type="text/css" href="'+"dynamicContent.css"+'" >').appendTo("head");
+	$('<link rel="stylesheet" type="text/css" href="'+"dynamicContent.css"+'" >').appendTo("head");
 };
 
 
