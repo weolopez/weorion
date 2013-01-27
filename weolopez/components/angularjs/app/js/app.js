583095bf90b603e374ad765fe5942fff7721e44d
weolopez <weolopez@yahoo.com>
2013-01-26 20:44:11 -0500

    updated angular

--- weolopez/components/angularjs/app/js/app.js
+++ weolopez/components/angularjs/app/js/app.js
@@ -5,7 +5,8 @@
 angular.module('weolopezApp', ['weolopezApp.filters', 'weolopezApp.services', 'weolopezApp.directives']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/enter', {templateUrl: 'partials/enter.html', controller: enterCtrl});
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: todoCtrl});
+    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: componentsCtrl});
+    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: componentsCtrl});
+    $routeProvider.when('/viewComponent', {templateUrl: 'partials/componentPartial.html', controller: componentsCtrl});
     $routeProvider.otherwise({redirectTo: '/enter'});
   }]);
--- weolopez/components/angularjs/app/js/services.js
+++ weolopez/components/angularjs/app/js/services.js
@@ -5,86 +5,41 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('weolopezApp.services', []).
-  value('version', '0.1');
+var weolopezModules = angular.module('weolopezApp.services', []);
+weolopezModules.value('version', '0.1');
+weolopezModules.value('localStorage', window.localStorage);
 
-angular.module('weolopezApp.services', [], function($provide ) {
-  $provide.factory('localStorage',window.localStorage);	
-
-    /**
-    * batchLog service allows for messages to be queued in memory and flushed
-    * to the console.log every 50 seconds.
-    *
-    * @param {*} message Message to be logged.
-    */
-    function batchLogModule($provide){
-    $provide.factory('batchLog', ['$timeout', '$log', function($timeout, $log) {
-    var messageQueue = [];
-     
-    function log() {
-    if (messageQueue.length) {
-    $log('batchLog messages: ', messageQueue);
-    messageQueue = [];
-    }
-    $timeout(log, 50000);
-    }
-     
-    // start periodic checking
-    log();
-     
-    return function(message) {
-    messageQueue.push(message);
-    }
-    }]);
-     
-    /**
-    * routeTemplateMonitor monitors each $route change and logs the current
-    * template via the batchLog service.
-    */
-    $provide.factory('routeTemplateMonitor',
-    ['$route', 'localStorage', '$rootScope',
-    function($route, localStorage, $rootScope) {
-  //  $rootScope.$on('$routeChangeSuccess', function() {
-  //  batchLog($route.current ? $route.current.template : null);
-  //  });
-    	var user = {
-   	   name: "lopez",
-       todos: undefined
-  	   };
-  	   return user;
-    }]);
-    }
-   
-     $provide.factory('user', function() {  	
-  	var user = {
-   	   name: "lopez",
-       todos: undefined
-  	};
-  	return user;
-  });
-  
-});  
-    // get the main service to kick of the application
-    angular.injector([localStorage]).get('user');
-
-
-/*
-
-angular.module('weolopezApp.services', []).
-  value('user', function($rootScope, localStorage) {
+weolopezModules.factory('userProfile', function($rootScope,localStorage) { 
 	
   var LOCAL_STORAGE_ID = 'wlUser',
       userString = localStorage[LOCAL_STORAGE_ID];
 
   var user = userString ? JSON.parse(userString) : {
-    name: "weo",
-    todos: undefined
+    name: undefined,
+    component: undefined
   };
-alert(JSON.parse(userString));
+
   $rootScope.$watch(function() { return user; }, function() {
     localStorage[LOCAL_STORAGE_ID] = JSON.stringify(user);
   }, true);
 
-  return user;
+	return user;
 });
-*/
+
+weolopezModules.factory('componentList', function($http) {
+  var componentArray = [];
+  var internallURL = 'http://home.orionhub.org:8080/weolopez/components/';
+    $http.get(internallURL).success(function (data) {
+                var loadedData = $(data);
+            	var compList = loadedData.find('a'); 
+                compList.each(function( index ) {
+                	var txt = $(this).text();
+                	txt = txt.substring(0,txt.length-1)
+					componentArray.push({text: txt, done:false});
+				}).error(function(data, status) {
+				    $scope.data = data || "Request failed";
+				    $scope.status = status;
+			    });
+    });
+    return componentArray;
+});
